80633e677e20fbb6263f6de557d02b0e52b24472
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-04-15 18:56:22 +1200

    [fix]: Number-string.
    Numbers were being taken as strings, put a plus sign before them to have them be considered as numbers.

--- src/functions/targeting.ts
+++ src/functions/targeting.ts
@@ -5,16 +5,15 @@ export const targetIncomeAsNZBusiness = (moneyWanted: number) => {
 }
 
 export const targetIncomeAsNZEmployee = (moneyWanted: number) => {
-    
-    if (moneyWanted + taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue1) {
-        return parseFloat((moneyWanted/(1-employeeTaxNZ1)).toFixed(2));
-    } else if (moneyWanted + taxNZLimitBracket2 + taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue2) {
-        return parseFloat((((moneyWanted + taxNZLimitBracket1) - employeeTaxNZBracketShiftValue1)/(1-employeeTaxNZ2) + employeeTaxNZBracketShiftValue1).toFixed(2));
-    } else if (moneyWanted + taxNZLimitBracket3 + taxNZLimitBracket2 + taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue3) {
-        return parseFloat(((((moneyWanted + taxNZLimitBracket2 + taxNZLimitBracket1) - employeeTaxNZBracketShiftValue2)/(1-employeeTaxNZ3)) + employeeTaxNZBracketShiftValue2).toFixed(2));
-    } else if (moneyWanted + taxNZLimitBracket4 + taxNZLimitBracket3 + taxNZLimitBracket2 + taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue4) {
-        return parseFloat((((moneyWanted + taxNZLimitBracket3 + taxNZLimitBracket2 + taxNZLimitBracket1) - employeeTaxNZBracketShiftValue3)/(1-employeeTaxNZ4) + employeeTaxNZBracketShiftValue3).toFixed(2));
+    if (+moneyWanted + +taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue1) {
+        return parseFloat((+moneyWanted/(1-+employeeTaxNZ1)).toFixed(2));
+    } else if (+moneyWanted + +taxNZLimitBracket2 + +taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue2) {
+        return parseFloat((((+moneyWanted + +taxNZLimitBracket1) - +employeeTaxNZBracketShiftValue1)/(1-+employeeTaxNZ2) + +employeeTaxNZBracketShiftValue1).toFixed(2));
+    } else if (+moneyWanted + +taxNZLimitBracket3 + +taxNZLimitBracket2 + +taxNZLimitBracket1 <= +employeeTaxNZBracketShiftValue3) {
+        return parseFloat(((((+moneyWanted + +taxNZLimitBracket2 + +taxNZLimitBracket1) - +employeeTaxNZBracketShiftValue2)/(1-+employeeTaxNZ3)) + +employeeTaxNZBracketShiftValue2).toFixed(2));
+    } else if (+moneyWanted + +taxNZLimitBracket4 + +taxNZLimitBracket3 + taxNZLimitBracket2 + taxNZLimitBracket1 <= employeeTaxNZBracketShiftValue4) {
+        return parseFloat((((+moneyWanted + +taxNZLimitBracket3 + +taxNZLimitBracket2 + +taxNZLimitBracket1) - +employeeTaxNZBracketShiftValue3)/(1-+employeeTaxNZ4) + +employeeTaxNZBracketShiftValue3).toFixed(2));
     } else {
-        return parseFloat((((moneyWanted + taxNZLimitBracket4 + taxNZLimitBracket3 + taxNZLimitBracket2 + taxNZLimitBracket1) - employeeTaxNZBracketShiftValue4)/(1-employeeTaxNZ5) + employeeTaxNZBracketShiftValue4).toFixed(2));
+        return parseFloat((((+moneyWanted + +taxNZLimitBracket4 + +taxNZLimitBracket3 + +taxNZLimitBracket2 + +taxNZLimitBracket1) - +employeeTaxNZBracketShiftValue4)/(1-+employeeTaxNZ5) + +employeeTaxNZBracketShiftValue4).toFixed(2));
     }
 }
