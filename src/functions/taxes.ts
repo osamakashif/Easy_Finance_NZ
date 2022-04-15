ec8fa32496081204d8896e6f0b9ade0e5ad766ce
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2022-04-15 18:44:34 +1200

    [fix]: Taxes 2d.p.
    Adjusted tax functions to output to 2 decimal places.

--- src/functions/taxes.ts
+++ src/functions/taxes.ts
@@ -1,19 +1,19 @@
 import { businessTaxNZ, employeeTaxNZ1, employeeTaxNZ2, employeeTaxNZ3, employeeTaxNZ4, employeeTaxNZ5, employeeTaxNZBracketShiftValue1, employeeTaxNZBracketShiftValue2, employeeTaxNZBracketShiftValue3, employeeTaxNZBracketShiftValue4, taxNZLimitBracket1, taxNZLimitBracket2, taxNZLimitBracket3, taxNZLimitBracket4 } from "./taxValuesNZ";
 
 export const calculateNZBusinessTax = (income: number) => {
-    return (income * businessTaxNZ);
+    return parseFloat((income * businessTaxNZ).toFixed(2));
 }
 
 export const calculateNZEmployeeTax = (income: number) => {
     if (income <= employeeTaxNZBracketShiftValue1) {
-        return (income * employeeTaxNZ1);
+        return parseFloat((income * employeeTaxNZ1).toFixed(2));
     } else if (income <= employeeTaxNZBracketShiftValue2) {
-        return (((income - employeeTaxNZBracketShiftValue1) * employeeTaxNZ2) + taxNZLimitBracket1);
+        return parseFloat((((income - employeeTaxNZBracketShiftValue1) * employeeTaxNZ2) + taxNZLimitBracket1).toFixed(2));
     } else if (income <= employeeTaxNZBracketShiftValue3) {
-        return (((income - employeeTaxNZBracketShiftValue2) * employeeTaxNZ3) + taxNZLimitBracket1 + taxNZLimitBracket2);
+        return parseFloat((((income - employeeTaxNZBracketShiftValue2) * employeeTaxNZ3) + taxNZLimitBracket1 + taxNZLimitBracket2).toFixed(2));
     } else if (income <= employeeTaxNZBracketShiftValue4) {
-        return (((income - employeeTaxNZBracketShiftValue3) * employeeTaxNZ4) + taxNZLimitBracket1 + taxNZLimitBracket2 + taxNZLimitBracket3);
+        return parseFloat((((income - employeeTaxNZBracketShiftValue3) * employeeTaxNZ4) + taxNZLimitBracket1 + taxNZLimitBracket2 + taxNZLimitBracket3).toFixed(2));
     } else {
-        return (((income - employeeTaxNZBracketShiftValue4) * employeeTaxNZ5) + taxNZLimitBracket1 + taxNZLimitBracket2 + taxNZLimitBracket3 + taxNZLimitBracket4);
+        return parseFloat((((income - employeeTaxNZBracketShiftValue4) * employeeTaxNZ5) + taxNZLimitBracket1 + taxNZLimitBracket2 + taxNZLimitBracket3 + taxNZLimitBracket4).toFixed(2));
     }
 }
